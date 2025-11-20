9ca55c46dab85be25b62353a3c31674ba4088f18
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2025-11-20 21:09:49 +0530

    MongoDb Connection Establishment

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+import connectDB from "./src/config/db.js";
 import app from "./src/main/app.js";
 
 const PORT = 3000;
+
+// Connect to MongoDB
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
---
+++ src/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+// Function to connect to MongoDB with retry logic
+async function connectDB(retries = 10, delay = 5000) {
+  while (retries) {
+    try {
+      await mongoose.connect("mongodb://localhost:27017/notes");
+      console.log("MongoDB Connected Successfully!");
+      return;
+    } catch (error) {
+      retries--;
+      console.log(`MongoDB connection failed, Retries left: ${retries}`);
+      await new Promise((res) => setTimeout(res, delay));
+    }
+  }
+  throw new Error("MongoDB connection failed after maximum retries!");
+}
+
+export default connectDB;
