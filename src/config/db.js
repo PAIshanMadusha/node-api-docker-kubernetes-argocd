ec48cd69f3ec0f8f6b574d98b3b13157232a8825
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2025-11-22 19:16:26 +0530

    Development of docker-compose.yml

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/notebook";
+// Below is the MongoDB connection URI for local development
+// e.g., "mongodb://localhost:27017/notebook"
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://mongo:27017/notebook"; // mongo is the service name in docker-compose.yml
 
 // Function to connect to MongoDB with retry logic
 async function connectDB(retries = 10, delay = 5000) {
