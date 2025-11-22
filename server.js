0a64584e33c615c0fee9b9b5e1da7ff6f06bfe8f
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2025-11-23 00:27:45 +0530

    Improved Version

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 import connectDB from "./src/config/db.js";
 import app from "./src/main/app.js";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Connect to MongoDB
-connectDB();
+async function startServer() {
+  // MongoDB Connection
+  await connectDB();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  app.listen(PORT, () => {
+    // Server start log
+    console.log(`Server is running on http://localhost:${PORT}`);
+
+    // Use process.env.HOSTNAME which is set by Kubernetes or Docker
+    // Local environment will default to "local-environment" if HOSTNAME is not set
+    console.log(
+      `Node API running on pod: ${process.env.HOSTNAME || "local-environment"}`
+    );
+  });
+}
+
+startServer();
